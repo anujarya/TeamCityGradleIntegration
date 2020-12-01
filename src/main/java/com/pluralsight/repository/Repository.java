ffef9b35ca08e910fddbf35cea433fd76ccff19d
anujarya <2247276+anujarya@users.noreply.github.com>
2020-12-01 17:46:15 +0530

    Adding the build and java files

---
+++ src/main/java/com/pluralsight/repository/Repository.java
@@ -0,0 +1,9 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.repository.BaseRepository;
+
+public class Repository extends BaseRepository{
+    public static void main(String[] args) {
+        
+    }
+}
